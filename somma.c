f9df2399c1ad0e6f6c6e44ef862ae2329bc5af84
magemma <martini.gemma3@gmail.com>
2026-03-04 17:21:49 +0100

    added C file

---
+++ somma.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int risultato; // ERRORE: variabile non inizializzata
+
+    risultato = risultato + a + b; 
+
+    printf("La somma di %d e %d e': %d\n", a, b, risultato);
+
+    return 0;
+}
