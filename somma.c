68f610e75fd81835e775f73a2f5bbe6f417d5c1e
Fulinik <fuligni.nikko@itcgcerboni.com>
2026-03-05 13:43:21 +0100

    file debuggato: fuligni

--- somma.c
+++ somma.c
@@ -2,10 +2,9 @@
 
 int main() {
     int a = 5;
-    int b = 10;
-    int risultato; // ERRORE: variabile non inizializzata
+    int b = 10;    int risultato; // ERRORE: variabile non inizializzata
 
-    risultato = risultato + a + b; 
+    risultato = a + b; 
 
     printf("La somma di %d e %d e': %d\n", a, b, risultato);
 
